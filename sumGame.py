41455b8a95ac20c9760ee6eaa48a5638c56397e3
LFRezende <lfsrsprofessional@gmail.com>
2026-01-31 20:58:26 -0300

    First Commit

---
+++ sumGame.py
@@ -0,0 +1,21 @@
+"""
+    Sum Game - GIT TRAINING
+
+    This game is meant to train git branching, rebasing, reflogging...
+    So to be cracked at it.
+
+    For it to work, we'll start with a base main branch.
+    Each branch will create a series of commits which create 
+    math operations, which envision to nullify another branch. 
+
+    Each branch must have at least three commits, each named
+    in the conventional, accepted way. 
+
+    
+
+"""
+
+# Main variable:
+a = 3
+b = 7
+c = 12
